5fb3de2e6c2975bde56178efdc81d37a0597cc94
mtsteo <mateusteotonho@gmail.com>
2023-07-01 00:21:58 -0300

    Agente Vinicius

---
+++ Utils/AgentConfig.js
@@ -0,0 +1,15 @@
+export const AgentConfig = {
+  type: "service_account",
+  project_id: "chatb--ljcw",
+  private_key_id: "924f1db298122299c77bdd88aec62954fe02c779",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCL297j7c8e4ysF\n+heKKrrhmzpciMajrZ2LE5A0KgkZt//8ZjpZUEaCUQ3AwGVown9Gvgd/2OQ7lCAt\n4KwGSJJiCN+Np95xPAXOFLDZRkHffqhNOscZySm9LpxzT1Y7PXOz4IsT3rszs/od\nZeHKefi93IjmMjgGUcaEtLR/9uNpDawvToxeuALeCQYDNVCwG00SN3Yt5YG/WFs0\nBrLB1Tu5eKnZdN9IjTr/Nf+VXQNbkK8o+zic8w9EojXAm77+VWzgfWl3BWHCrmiM\nW4Zl6bIA7n6Ihg+3RWpMnA1wILy9wBW0D6vhkrYBwkCqgWfq3z0k/MWugZ/qfccG\niCoVKIMRAgMBAAECggEAHU5Gn6MSjenS948mhJSckS6KJZ6hZOW5x80jH9MZEhMM\nqDIxkspv49mhFJ0cAr4Sllu2TZNAi71nmsVjtnNZfQJX+aJNyZNmsf6C6hw/LFop\nYPlqg9wzi0krjjv3GTWwu2XXLqQyvMw/Ig7Zp6g0N3BxHS8zrxtOijslQVUV5Mqh\nxTquYOnzR568EaPE5IaZWO5tpH3QhDEAidxcoBpmU4rY/xBX7C2NDlW0mKH6zFKh\n2SD7vZkMxKln6bW1fJuYYl3jgn+/Q2dFCAh6NbM04rylC4BnOjQrd3vcrL5GJn5i\nKxswlgLhupEelbeV9yh6GIJxTgMA6dkrQPS8QBM9awKBgQDFgtEjKWyHyLerSVih\nXEE7cokK9Hhl9GWhlAIFbbpOfSRaZ4X58lGpRrZsJYGs2vJNY5jq9vSHzZLCb0sO\neIm8BMq34mM576lMbHOFO1ioHuaAwP1+hzn1Cy1gR65t/YMOtBnbz6pRTiuGEc5r\nul/nyR5zH88lbPAE2h6IgiEqiwKBgQC1Rnt1/EW+qw8DflnMp7QKrdgSo7RbmDa+\nwRAa1O37fQ0BGqU7i8NvuFD9t6xe820ZThj6UUx0b+OowwdPszJOLY8SQBQIug7O\nC/MvZ3sFWqAJ7mS7rg6At65fcn49jHzh1l9sx4b0+0UOTTvJDNYk4X4wi81L14I8\nYzOOc4YoUwKBgA1SGVDJxmwR+XEv7S5UzoF35A49FrGpngEbvlcnY11Py1fdi4ej\n9CX6gusgTHKgAZ9+jB2ssilGkBNU9jbvojpwCEwGlLKfm0dwOQpPSldsXhdo7K/P\n5vTD4DeSHg2E/4XKV8QfaVk6cG0GXmmdlEJJqigRiSL5CecJ3u1N+XapAoGBAKBz\n76oCj1Vyogpsl12//2wIbp4pCuacN7DmwLOPT8rv6EfW5vRrBKVp1z6MimVNQH9K\nkm9W8L6Ln05supcAPYHXb7EuyMzkq2AUUb/e75RQoCHKQp2hNjMW4qCXfkhKhut7\nTeHCRqGnm3tKP6X417+zXHfp9uCvtBZxlFv9w30DAoGAUeEnZHTurL/nkSiDQ7CG\n5LSgi1M1p0jsj+LCD+A2KfCjkourLHpow0QzBeua2gb+5ZKd7iGBDzGBQAyzKjRI\nwFusKISDtSz1opzhHz+jRvbnQb/UfVSc9qGA/ui0XhMbpxGMCxGa4/UoPj01W2RX\nG2JCk7wm1g38aXKRx0N0mXg=\n-----END PRIVATE KEY-----\n",
+  client_email: "chat02@chatb--ljcw.iam.gserviceaccount.com",
+  client_id: "100728175287759839298",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/chat02%40chatb--ljcw.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
